49a6c6831f88fbc626f80bf5af5593be37abd77c
swgoodman <spencerwgoodman@icloud.com>
2019-03-26 16:37:35 -0700

    Setting up client websocket

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const app = express()
+const app = express();
 
 // Set the template engine ejs
 app.set('view engine', 'ejs')
@@ -9,8 +9,15 @@ app.use(express.static('public'))
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render('index')
 })
 
 // Listening Port
 server = app.listen(3000)
+
+// Websocket Configuration
+const io = require("socket.io")(server)
+
+io.on('connection', (socket) => {
+    console.log('New user connected')
+})
