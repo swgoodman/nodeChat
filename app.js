1dcaabfb715a79ce179cefc81817746d602cf1cf
swgoodman <spencerwgoodman@icloud.com>
2019-03-26 16:53:25 -0700

    Listening for username change

--- app.js
+++ app.js
@@ -20,4 +20,11 @@ const io = require("socket.io")(server)
 
 io.on('connection', (socket) => {
     console.log('New user connected')
+    // Default username
+    socket.username = "Anonymous"
+    // Custom username
+    socket.on('change_username', (data) => {
+        socket.username = data.username
+    })
+
 })
